d0046ede9c45a60536c8b7b1c11f83aa47009227
Pablo <psalazar5@live.com>
2023-03-23 14:57:34 -0400

    Assignment

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,24 +1,26 @@
 window.alert("Hello, World!");
 var A = "This is a string";
 document.write(A);
+document.write("<br></br>");
 
 var B = "This is a string2";
 document.write(B);
+document.write("<br>");
 
 document.write(
-  'Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!"<br> "Eat my shorts!"Bart responded.'
+  'Lisa told Bart ," Knock it off, Bart! Or I\'ll tell dad!"<br> "Eat my shorts!" Bart responded.'
 );
-
+document.write("<br>");
 document.write(
   '"Be who you are and say what you feel,' +
     "because those who mind don't matter and those who matter don't mind.\"" +
     "-Dr. Seuss"
 );
-
-var C =
-  "This is the beginning of the string" + "and this is the end of the string";
-document.write(C);
-
+document.write("<br>");
+var C = "This is the beginning of the string";
+var D = "and this is the end of the string";
+document.write(C + D);
+document.write("<br>");
 var Family = "The Arezzinis",
   Dad = "Jeremiah",
   Mom = "Hermoine",
---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_Second_Function() {
+  var str = "Wow";
+  var str = "Why?";
+  document.getElementById("button").innerHTML = str;
+}
+
+function myFunction() {
+  var sentence = "I am learning";
+  sentence += "a lot from this book!";
+  document.getElementById("concatenate").innerHTML = sentence;
+}
